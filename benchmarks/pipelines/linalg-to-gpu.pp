adf0325eedddb349adf30366341538d24005bbfd
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-12-08 08:14:52 -0800

    Modified benchmark pipeline for GEMM kernels

--- benchmarks/pipelines/linalg-to-gpu.pp
+++ benchmarks/pipelines/linalg-to-gpu.pp
@@ -1,48 +1,58 @@
 // linalg dialect to gpu dialect lowering pipeline
 // Ready for vulkan runner or narrow scope l0/sycl runner starting from GPU dialect.
-builtin.module(convert-tensor-to-linalg
-    arith-bufferize
-    func.func(empty-tensor-to-alloc-tensor
-          // eliminate-empty-tensors
-          scf-bufferize
-          shape-bufferize
-          linalg-bufferize
-          bufferization-bufferize
-          tensor-bufferize)
-    func-bufferize
-    func.func(finalizing-bufferize
-          convert-linalg-to-parallel-loops
-          imex-add-outer-parallel-loop
-          gpu-map-parallel-loops
-          convert-parallel-loops-to-gpu)
-// insert-gpu-allocs pass can have client-api = opencl or vulkan args
-    func.func(insert-gpu-allocs{client-api=opencl})
-    canonicalize
-    normalize-memrefs
-// Unstride memrefs does not seem to be needed.
-//  func.func(unstride-memrefs)
-    func.func(lower-affine)
-    gpu-kernel-outlining
-    canonicalize
-    cse
-// The following set-spirv-* passes can have client-api = opencl or vulkan args
-    set-spirv-capabilities{client-api=opencl}
-    gpu.module(set-spirv-abi-attrs{client-api=opencl})
-    canonicalize
+//    xetile-tiling
+//    convert-xetile-to-xegpu
+//    canonicalize
+builtin.module(
+//    canonicalize
+//    xetile-tiling
+//    convert-xetile-to-xegpu
+// NOTE: comment out canonicalization for integration test:
+//       XeGPU/gemm_4kx4kx4k_f16_f16_f32.mlir
+   canonicalize
     fold-memref-alias-ops
-    imex-convert-gpu-to-spirv
+    arith-expand
+    lower-affine
+    imex-convert-gpu-to-spirv{enable-vc-intrinsic=true}
     spirv.module(spirv-lower-abi-attrs
              spirv-update-vce)
     func.func(llvm-request-c-wrappers)
     serialize-spirv
+    convert-vector-to-scf
     convert-gpu-to-gpux
     convert-scf-to-cf
+    convert-cf-to-llvm
+    convert-vector-to-llvm
+    convert-index-to-llvm
+    convert-arith-to-llvm
     convert-func-to-llvm
     convert-math-to-llvm
     convert-gpux-to-llvm
-    convert-cf-to-llvm
+    convert-index-to-llvm
     expand-strided-metadata
     lower-affine
     finalize-memref-to-llvm
-    reconcile-unrealized-casts)
+    reconcile-unrealized-casts
+    )
 // End
+// builtin.module(
+//     imex-convert-gpu-to-spirv{enable-vc-intrinsic=true}
+//     spirv.module(spirv-lower-abi-attrs
+//              spirv-update-vce)
+//     func.func(llvm-request-c-wrappers)
+//     serialize-spirv
+//     convert-vector-to-scf
+//     convert-gpu-to-gpux
+//     convert-scf-to-cf
+//     convert-cf-to-llvm
+//     convert-vector-to-llvm
+//     convert-index-to-llvm
+//     convert-arith-to-llvm
+//     convert-func-to-llvm
+//     convert-math-to-llvm
+//     convert-gpux-to-llvm
+//     convert-index-to-llvm
+//     expand-strided-metadata
+//     lower-affine
+//     finalize-memref-to-llvm
+//     reconcile-unrealized-casts)
