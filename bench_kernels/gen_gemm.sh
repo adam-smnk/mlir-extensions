184861189f9008e8b03e1948932b4332c81ee191
Adam Siemieniuk <adam.siemieniuk@intel.com>
2024-03-08 10:13:21 -0800

    TPP+IMEX matmul benchmarks

---
+++ bench_kernels/gen_gemm.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+IMEX_ROOT=$(realpath ~/mlir-extensions)
+TEMPLATE_FILE=$(realpath ${IMEX_ROOT}/bench_kernels/gemm_M_N_K.mlir)
+
+# Initial validation.
+if ! [ -d ${IMEX_ROOT} ]; then
+  echo "Missing IMEX repo"
+  exit 1
+fi
+if ! [ -f ${TEMPLATE_FILE} ]; then
+  echo "Missing template MLIR file"
+  exit 1
+fi
+
+OUT_DIR=$(dirname ${TEMPLATE_FILE})
+
+# Kernel config.
+MBS=( 128 256 512 )
+SIZES=( 1024 2048 4096 8192 )
+
+# Generate files.
+echo "Generating kernels..."
+for MB in "${MBS[@]}"; do
+  for SIZE in "${SIZES[@]}"; do
+    OUT_FILE="gemm_${MB}_${SIZE}_${SIZE}.mlir"
+    echo ${OUT_FILE}
+    exec sed -E "s/@DIMS_A@/${MB}x${SIZE}/g" ${TEMPLATE_FILE} | \
+         sed -E "s/@DIMS_B@/${SIZE}x${SIZE}/g" | \
+         sed -E "s/@DIMS_C@/${MB}x${SIZE}/g" > \
+         ${OUT_DIR}/${OUT_FILE}
+  done
+done
---
+++ bench_kernels/run_bench.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+LLVM_ROOT=$(realpath ~/llvm-project)
+
+IMEX_ROOT=$(realpath ~/mlir-extensions)
+IMEX_RUNNER=$(realpath ${IMEX_ROOT}/build/bin/imex-runner.py)
+KERNELS_DIR=$(realpath ${IMEX_ROOT}/bench_kernels)
+
+TPP_ROOT=$(realpath ~/tpp-mlir)
+TPP_RUN=$(realpath ${TPP_ROOT}/build/bin/tpp-run)
+
+CONFIG_FILE=$(realpath ${KERNELS_DIR}/gemm_benchmarks.txt)
+
+# Initial validation.
+if ! { [ -d ${IMEX_ROOT} ] || [ -f ${IMEX_RUNNER} ]; }; then
+  echo "Missing IMEX repo"
+  exit 1
+fi
+if ! [ -d ${KERNELS_DIR} ]; then
+  echo "Missing kernels directory"
+  exit 1
+fi
+if ! { [ -d ${TPP_ROOT} ] || [ -f ${TPP_RUN} ]; }; then
+  echo "Missing TPP repo"
+  exit 1
+fi
+if ! [ -f ${CONFIG_FILE} ]; then
+  echo "Missing bench config file"
+  exit 1
+fi
+
+# Benchmark config.
+TPP_RUN_FLAGS="-gpu=intel -entry-point-result=void -e entry"
+
+LIB_MLIR=$(realpath ${LLVM_ROOT}/build/lib/libmlir_runner_utils.so)
+LIB_IMEX=$(realpath ${IMEX_ROOT}/build/lib/libimex_runner_utils.so)
+LIB_SYCL=$(realpath ${IMEX_ROOT}/build/lib/libsycl-runtime.so)
+
+IMEX_SHARED_LIBS="--shared-libs=${LIB_MLIR},${LIB_IMEX},${LIB_SYCL}"
+IMEX_RUNNER_FLAGS="--requires=sycl-runtime --runner imex-cpu-runner -e entry --entry-point-result=void"
+
+IMEX_ENV="IMEX_ENABLE_LARGE_REG_FILE=1 IMEX_ENABLE_PROFILING=1"
+
+PIPELINE_FILE=$(realpath ${IMEX_ROOT}/benchmarks/pipelines/linalg-to-gpu.pp)
+
+# Run benchmarks.
+while read LINE || [[ -n $LINE ]]; do
+  TOKENS=(${LINE})
+  FILE=${KERNELS_DIR}/${TOKENS[0]}
+  FLAGS=${TOKENS[@]:1}
+
+  MLIR_XEGPU=$(exec ${TPP_RUN} ${FILE} ${TPP_RUN_FLAGS} ${FLAGS} 2>&1 | sed '/Error:.*/d' )
+  echo "${MLIR_XEGPU}" | \
+    exec env ${IMEX_ENV} python ${IMEX_RUNNER} ${IMEX_RUNNER_FLAGS} ${IMEX_SHARED_LIBS} \
+         --pass-pipeline-file=${PIPELINE_FILE} 2>/dev/null | \
+    sed -nE "s/.*kernel execution time.*avg\: ([0-9.]+).*/\\1/p"
+done < ${CONFIG_FILE}
